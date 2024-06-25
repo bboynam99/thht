4121089cf0e2181e819589eda07b134084ec9534
GoblinPunk1 <123496826+GoblinPunk1@users.noreply.github.com>
2024-06-26 01:06:14 +0300

    Add files via upload

---
+++ src/Mint.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { toNano } from '@ton/ton';
+import { beginCell } from '@ton/ton';
+import { TonConnectUIProvider, useTonConnectUI, useTonWallet } from '@tonconnect/ui-react';
+import styles from './Button.module.css'; // Import button styles
+
+const body = beginCell()
+    .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
+    .storeStringTail("MintRandom") // write our text comment
+    .endCell();
+
+const myTransaction = {
+    validUntil: Math.floor(Date.now() / 1000) + 360,
+    messages: [
+        {
+            address: process.env.NEXT_PUBLIC_CONTRACT,
+            amount: toNano(0.15).toString(),
+            payload: body.toBoc().toString("base64") // payload with comment in body
+        }
+    ]
+};
+
+export const Mint = () => {
+    const [error, setError] = useState(null); // Add state for errors
+    const [tonConnectUi] = useTonConnectUI();
+    const [tx] = useState(myTransaction);
+    const wallet = useTonWallet();
+
+    const handleTransaction = async () => {
+        try {
+            await tonConnectUi.sendTransaction(tx);
+        } catch (e) {
+            setError(e.message); // Set the error message
+        }
+    };
+
+    return (
+        <TonConnectUIProvider manifestUrl="https://tonconnect-test.vercel.app/tonconnect-manifest.json">
+            <div>
+                {wallet ? (
+                    <button 
+                        className={styles.button}
+                        onClick={handleTransaction}
+                    >
+                        Mint
+                    </button>
+                ) : (
+                    <button 
+                        className={styles.button}
+                        onClick={() => tonConnectUi.openModal()}
+                    >
+                        Connect Wallet
+                    </button>
+                )}
+                {error && (
+                    <div className="error-message">
+                        {error}
+                    </div>
+                )}
+            </div>
+        </TonConnectUIProvider>
+    );
+};
